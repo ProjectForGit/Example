aa9d79749d8f7ea1b5728d12eb089a8ce678bdc6
ProjectForGit <blacksilver@ro.ru>
2021-01-22 11:36:22 +0300

    First commit

---
+++ Upload to Git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upload_to_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
